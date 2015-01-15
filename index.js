96489960bbab3360613d428ecf5bb0db34a90cff
derris <derris@163.com>
2015-01-15 23:25:22 +0800

    init first 3

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Administrator on 2014/11/4.
+ */
+
+var express = require('express');
+var path = require('path');
+var app = express();
+app.set('port', 80);
+
+var ls_www = process.cwd();
+console.log(ls_www);
+app.use(express.static(ls_www + "\\public" ));
+
+var server = app.listen(app.get('port'), function() {
+  console.log( server.address());
+  console.log(ls_www );
+});
