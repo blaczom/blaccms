0546239cd339b2cc27749ad2820ede8eccdd3b7e
derris <derris@163.com>
2015-01-16 22:49:58 +0800

    ver 0.0.1 blaccms forground.

--- public/js/app.js
+++ public/js/app.js
@@ -30,5 +30,46 @@ myApp.config(function($stateProvider, $urlRouterProvider) {
       controller: function($scope) {
         $scope.things = ["A", "Set", "Of", "Things"];
       }
-    });
+    })
+    .state('thi', {
+      url: "/thi",
+      templateUrl: "partials/thi.html"
+    })
+    .state('thi.list', {
+      url: "/list",
+      templateUrl: "partials/thilist.html",
+      controller: function($scope) {
+        $scope.things = ["A33", "S33", "Of3", "3Things"];
+      }
+    }).state('fou', {
+      url: "/fou",
+      templateUrl: "partials/fou.html"
+    })
+    .state('fou.list', {
+      url: "/list",
+      templateUrl: "partials/foulist.html",
+      controller: function($scope) {
+        $scope.things = ["A4", "Set44", "Of44", "Things44"];
+      }
+    }).state('fiv', {
+      url: "/fiv",
+      templateUrl: "partials/fiv.html"
+    })
+    .state('fiv.list', {
+      url: "/list",
+      templateUrl: "partials/fivlist.html",
+      controller: function($scope) {
+        $scope.things = ["A5", "Set5", "Of5", "Things5"];
+      }
+    }).state('last', {
+      url: "/last",
+      templateUrl: "partials/last.html"
+    })
+    .state('last.list', {
+      url: "/list",
+      templateUrl: "partials/lastlist.html",
+      controller: function($scope) {
+        $scope.things = ["last", "last", "last", "Tlastgs5"];
+      }
+    })
 });
